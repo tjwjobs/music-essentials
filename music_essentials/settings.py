37a3b342ef6ad5a41441d99465ec581ea2edd105
tjwjobs <tjwjobs@yahoo.com>
2020-12-31 12:58:47 +0000

    changed settings

--- music_essentials/settings.py
+++ music_essentials/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&$n+46n3v)zha22%$68wieltt$!u2pv1g*_&41r4mj@pqk07-r'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['music-essentials.herokuapp.com', 'localhost']
 
